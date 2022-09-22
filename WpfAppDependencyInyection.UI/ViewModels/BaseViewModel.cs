27330901648923bd3da5e9e6be104a7ca9eae612
Marti <marllaest2075@hotmail.com>
2022-09-22 14:53:52 -0700

    Base
    Clase base para las viatamodelo

---
+++ WpfAppDependencyInyection.UI/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfAppDependencyInyection.UI.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = "")
+        {
+            if(Equals(field, newValue)) return false;
+            field = newValue;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
