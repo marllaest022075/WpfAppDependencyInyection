e0e86884d48039d5e772de42ee07af01bae720ba
Marti <marllaest2075@hotmail.com>
2022-09-23 09:47:03 -0700

    Administracion
    Vista de administracio

--- WpfAppDependencyInyection.UI/ViewModels/AdminViewModel.cs
+++ WpfAppDependencyInyection.UI/ViewModels/AdminViewModel.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using WpfAppDependencyInyection.UI.Helpers;
+using WpfAppDependencyInyection.UI.ViewModels;
 
 namespace WpfAppDependencyInyection.UI.ViewModels
 {
@@ -12,16 +14,29 @@ namespace WpfAppDependencyInyection.UI.ViewModels
     {
 
         private string _mensage;
+        private Visibility _isAdmin;
+        private readonly RelayCommand _selectionCommand;
 
         public string Mensage { get => _mensage; set => SetProperty(ref _mensage, value); }
+        public ICommand SelectionCommand => _selectionCommand;
+        public Visibility IsAdmin { get => _isAdmin; set => SetProperty(ref _isAdmin, value); }
+
 
         public AdminViewModel()
         {
             Width = 950;
             Heigth = 550;
             _mensage = "Administrador";
-            ShowIt = Visibility.Visible;
+            _isAdmin = Visibility.Visible;
+            _selectionCommand = new RelayCommand(Selection);
         }
+
+
+        private void Selection(object commandParameter)
+        {
+            var cad = (String)commandParameter;            
+        }
+
 
     }
 }
